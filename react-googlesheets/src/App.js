37855ca4e207518c3d18b83b9c68ca2682c82507
JonGudenzi <jongudenzi@gmail.com>
2021-11-01 22:17:51 -0400

    Addition of Axios Post request to add new trello card with data from Company_Name user input

--- react-googlesheets/src/App.js
+++ react-googlesheets/src/App.js
@@ -1,30 +1,33 @@
 import React, { useState } from 'react';
-import { Button, Form, Container, Header } from 'semantic-ui-react';
+import { Button, Form, Container, Header, } from 'semantic-ui-react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [ALU, setALU] = useState('');
-  const [Description, setDescription] = useState('');
+
+  const [Card_Post, setCard_Post] = useState('');
+  const [Date, setDate] = useState('');
+  const [Freight_Company, setFreight_Company] = useState('');
   const [Old_Price, setold_Price] = useState('');
   const [New_Price, setNew_Price] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const objt = { ALU, Description, Old_Price, New_Price, };
-    if (ALU.length === 0) {
-      alert('You are missing an ALU');
+    const objt = { Date, Freight_Company, Old_Price, New_Price, Card_Post};
+    if (Date.length === 0) {
+      alert('You are missing the Date!');
     }
-    else if (Description.length === 0) {
-      alert('You are missing a Description');
+    else if (Freight_Company.length === 0) {
+      alert('You are missing the Freight Company Name!');
     }
     else if (Old_Price.length === 0) {
-      alert('You are missing a Old Price');
+      alert('Please provide how many boxes');
     }
     else if (New_Price.length === 0) {
-      alert('You are missing a New Price');
+      alert('You are missing the company name');
     }
+    
 
     else {
 
@@ -36,27 +39,47 @@ function App() {
         .then((response) => {
           console.log(response);
           
-          
-        });
+        });   
+    } 
+
+    if (Card_Post.length === 0) {
+      alert('You are missing the Company Name!');
     }
+    else{
+
+      axios
+        .post(
+          `https://api.trello.com/1/cards?key=48f7b748bbe946f9ada9ffd9fad114ab&token=8ae340536776d2736422804003b661708a5fd174b18f7d88d7b6dd95f771233c&idList=617ebe7269143a62624c8a39&name=${Card_Post}`
+        )
+        .then((response) => {
+          console.log(response);
+          alert('Success!');
+        });   
+    
+      };
   };
+////////////////////////////////////////////////////
+
+
+
 
   return (
     <Container fluid className="container">
-      <Header as="h2">IE Price Change Request</Header>
+      <Header  as="h1">IE Incoming Shipment</Header>
       <Form className="form">
         <Form.Field>
-          <label>ALU</label>
+          <label>Date</label>
           <input
-            placeholder="Enter your Name"
-            onChange={(e) => setALU(e.target.value)}
+         
+            placeholder="Enter Date"
+            onChange={(e) => setDate(e.target.value)}
           />
         </Form.Field>
         <Form.Field >
-          <label>Description</label>
+          <label>Freight_Company</label>
           <input
-            placeholder="Enter your Age"
-            onChange={(e) => setDescription(e.target.value)}
+            placeholder="Enter Freight Company"
+            onChange={(e) => setFreight_Company(e.target.value)}
           />
         </Form.Field>
         <Form.Field>
@@ -74,6 +97,14 @@ function App() {
           />
         </Form.Field>
 
+        <Form.Field>
+          <label>Company</label>
+          <input
+            placeholder="Enter Company name"
+            onChange={(e) => setCard_Post(e.target.value)}
+          />
+        </Form.Field>
+
         <Button color="blue" type="submit" onClick={handleSubmit}>
           Submit
         </Button>
@@ -81,5 +112,6 @@ function App() {
     </Container>
   );
 }
+
 
 export default App;
