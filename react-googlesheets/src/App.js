f67c9091eb0ef0f8a92fee88e0a89261a3556e78
JonGudenzi <jongudenzi@gmail.com>
2021-11-07 13:23:53 -0500

    Correction of Form and APIs. Clear form after submit

--- react-googlesheets/src/App.js
+++ react-googlesheets/src/App.js
@@ -5,112 +5,111 @@ import './App.css';
 
 function App() {
 
-  const [Card_Post, setCard_Post] = useState('');
+  let APIkey = "48f7b748bbe946f9ada9ffd9fad114ab";
+  let APItoken = "8ae340536776d2736422804003b661708a5fd174b18f7d88d7b6dd95f771233c";
+
   const [Date, setDate] = useState('');
   const [Freight_Company, setFreight_Company] = useState('');
-  const [Old_Price, setold_Price] = useState('');
-  const [New_Price, setNew_Price] = useState('');
+  const [TotalBoxNumber, setTotalBoxNumber] = useState('');
+  const [VendorName, setVendorName] = useState('');
+  
+  const dateChangeHandler = (event) => {
+    setDate(event.target.value);
+};
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const objt = { Date, Freight_Company, Old_Price, New_Price, Card_Post};
+    const objt = { 
+      Date: Date,
+      Freight_Company: Freight_Company, 
+      TotalBoxNumber: TotalBoxNumber ,
+      VendorName: VendorName};
+
+      setDate('');
+      setFreight_Company('');
+      setTotalBoxNumber('');
+      setVendorName('');
+
+    const postToAPIS = () => {
+      axios
+        .post(
+          'https://sheet.best/api/sheets/43b98d36-2257-422f-b1ee-60a1a2f1b568',
+          objt
+        );
+
+        axios
+              .post(
+                `https://api.trello.com/1/cards?key=${APIkey}&token=${APItoken}&idList=6183456a546264705e648406&name=${VendorName}`
+              );
+      };
+
     if (Date.length === 0) {
       alert('You are missing the Date!');
     }
     else if (Freight_Company.length === 0) {
       alert('You are missing the Freight Company Name!');
     }
-    else if (Old_Price.length === 0) {
+    else if (TotalBoxNumber.length === 0) {
       alert('Please provide how many boxes');
     }
-    else if (New_Price.length === 0) {
-      alert('You are missing the company name');
-    }
-    
-
     else {
-
-      axios
-        .post(
-          'https://sheet.best/api/sheets/43b98d36-2257-422f-b1ee-60a1a2f1b568',
-          objt
-        )
-        .then((response) => {
-          console.log(response);
-          
-        });   
-    } 
-
-    if (Card_Post.length === 0) {
-      alert('You are missing the Company Name!');
+      postToAPIS(); 
+      alert('Success!');
     }
-    else{
-
-      axios
-        .post(
-          `https://api.trello.com/1/cards?key=48f7b748bbe946f9ada9ffd9fad114ab&token=8ae340536776d2736422804003b661708a5fd174b18f7d88d7b6dd95f771233c&idList=617ebe7269143a62624c8a39&name=${Card_Post}`
-        )
-        .then((response) => {
-          console.log(response);
-          alert('Success!');
-        });   
-    
-      };
   };
-////////////////////////////////////////////////////
-
-
-
 
   return (
-    <Container fluid className="container">
-      <Header  as="h1">IE Incoming Shipment</Header>
-      <Form className="form">
+    <Container fluid className="container" >
+      <Header as="h1">IE Incoming Shipment</Header>
+      <Form className="form" onSubmit={handleSubmit}>
         <Form.Field>
           <label>Date</label>
           <input
-         
+          value={Date}
             placeholder="Enter Date"
-            onChange={(e) => setDate(e.target.value)}
+            onChange={dateChangeHandler}
           />
         </Form.Field>
+
         <Form.Field >
-          <label>Freight_Company</label>
+          <label>Freight Company</label>
           <input
+          value={Freight_Company}
             placeholder="Enter Freight Company"
             onChange={(e) => setFreight_Company(e.target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Old Price</label>
-          <input
-            placeholder="Enter your Old Price Here"
-            onChange={(e) => setold_Price(e.target.value)}
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>New Price</label>
-          <input
-            placeholder="Enter your New Price"
-            onChange={(e) => setNew_Price(e.target.value)}
+            
           />
         </Form.Field>
 
         <Form.Field>
-          <label>Company</label>
+          <label>Total Boxes</label>
           <input
-            placeholder="Enter Company name"
-            onChange={(e) => setCard_Post(e.target.value)}
+          value={TotalBoxNumber}
+            placeholder="Enter Total Box Amount"
+            onChange={(e) => setTotalBoxNumber(e.target.value)}
           />
         </Form.Field>
-
-        <Button color="blue" type="submit" onClick={handleSubmit}>
+        
+        <Form.Field>
+          <label>Vendor Name</label>
+          <input
+          value={VendorName}
+            placeholder="Enter Vendor name"
+            onChange={(e) => setVendorName(e.target.value)}
+          />
+        </Form.Field>
+        <Button color="blue" type="submit" >
           Submit
         </Button>
+
       </Form>
+      
     </Container>
+    
   );
+
+  
 }
 
 
